467981426b69032b3326b941a326eac22ad18574
Nugraha270203 <Nugrahamuhammadtaufik2003@gmail.com>
2024-01-20 13:19:32 +0700

    sa

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,12 @@
+import bg1 from "../assets/bg1.jpg"
 function Navbar() {
     return (
             <nav class="navbar navbar-expand-lg bg-dark shadow fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand text-white" href="#">Navbar</a>
+                    <a class="navbar-brand text-white" href="#">
+                    <img src={bg1}>
+                    </img>
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
--- src/component/Video.js
+++ src/component/Video.js
@@ -4,93 +4,83 @@ function Video() {
     return (
         <div className="container text-white">
             <h1 style={{ borderBottom: "2px solid white" }}>Terbaru!</h1>
-            
+
             <div className="row">
                 <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "17rem"}}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    <a href="">
+                        <div class="card" style={{ width: "17rem" }}>
+                            <img src={foto1} class="card-img"></img>
+                            <div class="card-img-overlay">
+                                <div className=" text-center ">
+                                    <div className="align-bottom">
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <h5 class="card-title text-white ">GTA V 1.20v</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "17rem"}}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </a>
+                </div> <div className="col-md-3 mb-3">
+                    <a href="">
+                        <div class="card" style={{ width: "17rem" }}>
+                            <img src={foto1} class="card-img"></img>
+                            <div class="card-img-overlay">
+                                <div className=" text-center ">
+                                    <div className="align-bottom">
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <h5 class="card-title text-white ">GTA V 1.20v</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "17rem"}}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </a>
+                </div> <div className="col-md-3 mb-3">
+                    <a href="">
+                        <div class="card" style={{ width: "17rem" }}>
+                            <img src={foto1} class="card-img"></img>
+                            <div class="card-img-overlay">
+                                <div className=" text-center ">
+                                    <div className="align-bottom">
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <h5 class="card-title text-white ">GTA V 1.20v</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "17rem" }}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </a>
+                </div> <div className="col-md-3 mb-3">
+                    <a href="">
+                        <div class="card" style={{ width: "17rem" }}>
+                            <img src={foto1} class="card-img"></img>
+                            <div class="card-img-overlay">
+                                <div className=" text-center ">
+                                    <div className="align-bottom">
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
+                                        <h5 class="card-title text-white ">GTA V 1.20v</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "16rem"}}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "16rem"   }}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "16rem" }}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3 mb-3">
-                    <div class="card" style={{ width: "16rem", marginRight: "10px" }}>
-                        <img src={foto1} class="card-img"></img>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-                
+
+
 
             </div>
 
-           
+
         </div>
     )
 }
